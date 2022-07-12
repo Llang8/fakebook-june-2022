9ba0c13941f40f60d5b3950f8428585ae871cf5c
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-07-12 10:58:25 -0500

    Added environment variable config and setup blueprint structure

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# Getting the path to the current project
+basedir = os.path.abspath(os.path.dirname(__name__)) 
+
+# Taking the path, and finding the absolute path to our .env file
+# and loading the variables into our environment
+# basedir + '\\' + '.env'
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config:
+    # FLASK_APP = 'development'
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
