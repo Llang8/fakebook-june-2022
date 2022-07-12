6c314072bab685ceed963467fb221a83428cefc6
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-07-12 15:15:30 -0500

    Added database models

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
 
     # Connect our config class to the application instance
     app.config.from_object(Config)
 
-    db = SQLAlchemy()
     migrate = Migrate()
 
     db.init_app(app)
