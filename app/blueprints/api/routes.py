1e5ee8d00f5ca5b94bc6706897fb0e3a7d0c1392
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-07-26 13:21:29 -0500

    Added new api routes for posts

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -23,6 +23,38 @@ def users():
 
     return jsonify(user_dict)
 
+@app.route('/posts')
+def posts():
+    result_posts = Post.query.all()    
+    
+    # Sort the queried posts by their date created
+    # Reverse it to make it go from newest to oldest
+    result_posts.sort(key=lambda post: post.date_created, reverse=True)
+
+    result_posts = list(map(lambda post: {
+        "id": post.id,
+        "body": post.body,
+        "date_created": post.date_created,
+        "user_id": post.user_id
+    }, result_posts))
+
+    print(result_posts)
+
+    return jsonify(result_posts)
+
+@app.route('/post/<id>')
+def post(id):
+    result_post = Post.query.get(id)
+
+    result_post = {
+        "id": result_post.id,
+        "body": result_post.body,
+        "date_created": result_post.date_created,
+        "user_id": result_post.user_id
+    }
+
+    return jsonify(result_post)
+
 @app.route("/status-update", methods=["POST"])
 def status_update():
     # Retrieve form data from request
@@ -41,4 +73,6 @@ def status_update():
     # Once the post is added to the database, send the user back to the homepage
     return redirect(url_for('main.home'))
 
-    
+
+
+
